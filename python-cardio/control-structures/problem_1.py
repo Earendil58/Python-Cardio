11886828c167472ec150814e5f048af80d7e5588
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2023-10-06 16:26:39 -0300

    first commit

---
+++ python-cardio/control-structures/problem_1.py
@@ -0,0 +1,5 @@
+# Diseña un programa que lea un número flotante por teclado y muestre por pantalla
+# el mensaje « El número es negativo » solo si el número es menor que cero.
+
+number = float(input('Enter a number: '))
+
