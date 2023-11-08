32749004106ddb0f7ee307694c257acc295b1995
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2023-11-07 23:12:09 -0300

    Problem 11 control structures

---
+++ control-structures/problem_11.py
@@ -0,0 +1,9 @@
+
+
+student_grade = int(input('Please, enter your grade: '))
+
+if student_grade == 10: 
+    print('Honor student')
+    
+if student_grade < 10 and student_grade >= 8:
+    print('Very good, your are awesome')
