8c6fd9057c074e39ef02fda65bbc76550aa5a31c
Gonzalo Aguirre <gonzalo5aguirre9@gmail.com>
2023-11-07 23:17:48 -0300

    final commit from problem 11 control structures

--- control-structures/problem_11.py
+++ control-structures/problem_11.py
@@ -5,5 +5,16 @@ student_grade = int(input('Please, enter your grade: '))
 if student_grade == 10: 
     print('Honor student')
     
-if student_grade < 10 and student_grade >= 8:
+elif student_grade < 10 and student_grade >= 8:
     print('Very good, your are awesome')
+    
+elif student_grade < 8 and student_grade >= 6 :
+    print('remarkable')
+    
+elif student_grade < 6 and student_grade >= 3:
+    print('You need to study more')
+    
+elif student_grade < 3 :
+    print('Come on! study harder')
+    
+    
